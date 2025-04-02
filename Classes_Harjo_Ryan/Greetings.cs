a17a1e45cea1d219836c3f46fdab280587a4e2b5
Ryan Harjo <ryanharjo01@gmail.com>
2025-04-02 14:15:15 -0500

    Add files via upload

---
+++ Classes_Harjo_Ryan/Greetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Harjo_Ryan
+{
+    internal class Greetings
+    {
+        public void Welcome()    // This method prints a welcome message to the user
+        {
+            Console.WriteLine("Hello User, Welcome!");    // Output a general welcome message to the console
+        }
+        public void Hello(string name)    // This method accepts a string parameter 'name' and prints a personalized message
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");  // Output a personalized thank you message with the provided name
+        }
+    }
+}
---
+++ Classes_Harjo_Ryan/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes_Harjo_Ryan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings greetings = new Greetings();   // Create an object of the Greetings class
+            greetings.Welcome();     // Call the Welcome method to print a general greeting message
+
+            Console.WriteLine("What is your name?");    // Ask the user to enter their name
+            string userName = Console.ReadLine();       // Read the user's input (the name) from the console and store it in the userName variable
+
+            greetings.Hello(userName);   // Call the Hello method, passing the user input (name) to personalize the greeting
+        }
+    }
+}
